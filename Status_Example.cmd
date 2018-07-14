454888c38cd00512aa72af83b0256d56fcf90914
Zzzzzzyt <37258146+Zzzzzzyt@users.noreply.github.com>
2018-07-14 15:38:34 +0800

    Basic Files

---
+++ Status_Example.cmd
@@ -0,0 +1,2 @@
+java -jar MovingStatus.jar "Zzzyt gwq2017 XIZCM bp_ZHD_qd htc001 Ling2Feng1 zhuky.rlg i_love_hellholestudios"
+@pause
---
+++ Submit_Example.cmd
@@ -0,0 +1,2 @@
+java -jar SubmitNoticer.jar "Zzzyt gwq2017 XIZCM bp_ZHD_qd htc001 Ling2Feng1 zhuky.rlg i_love_hellholestudios" 10
+@pause
